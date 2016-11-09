b4bb2b18fda86958c74fb301fafe8cbc721ec6c3
Hicham El Hammouchi <hicham.elhammouchi@gmail.com>
2016-11-09 09:19:14 -0500

    Upgraded phone-list component to ng2
    - Used upgrade adapter’s downgradeNg2Component to add it to hybrid application

--- app/main.ts
+++ app/main.ts
@@ -4,11 +4,19 @@ import { UpgradeAdapter } from '@angular/upgrade';
 import { AppModule } from './app.module';
 
 import { Phone } from './core/phone/phone.service';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
 const upgradeAdapter = new UpgradeAdapter(AppModule);
 
-
 angular.module('core.phone')
     .factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
+
+// The as angular.IDirectiveFactory cast tells the TypeScript compiler
+// that the return value of the downgrade method is a directive factory
+angular.module('phoneList')
+.directive(
+    'phoneList',
+    upgradeAdapter.downgradeNg2Component(PhoneListComponent) as angular.IDirectiveFactory
+);
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,24 +1,55 @@
+import { Component } from '@angular/core';
 import { Phone, PhoneData } from '../core/phone/phone.service';
-declare var angular: any;
 
-class PhoneListController {
+@Component({
+  moduleId: module.id,
+  selector: 'phone-list',
+  templateUrl: 'phone-list.template.html'
+})
+
+export class PhoneListComponent {
   phones: PhoneData[];
+  query: string;
   orderProp: string;
-
-  static $inject = ['phone']; // The dependency injection annotations are attached to the class using a static property $inject
 
   constructor(phone: Phone) {
     phone.query().subscribe(phones => {
       this.phones = phones;
     });
+
     this.orderProp = 'age';
   }
-}
 
-angular.
-module('phoneList').
-component('phoneList', {
-  templateUrl: 'phone-list/phone-list.template.html',
-  controller: PhoneListController
-});
+  getPhones(): PhoneData[] {
+    return this.sortPhones(this.filterPhones(this.phones));
+  }
+
+  private filterPhones(phones: PhoneData[]) {
+    if (phones && this.query) {
+      return phones.filter(phone => {
+        let name = phone.name.toLowerCase();
+        let snippet = phone.snippet.toLowerCase();
+        return name.indexOf(this.query) >= 0 || snippet.indexOf(this.query) >= 0;
+      });
+    }
+    return phones;
+  }
+
+  private sortPhones(phones: PhoneData[]) {
+    if (phones && this.orderProp) {
+      return phones
+          .slice(0) // Make a copy
+          .sort((a, b) => {
+            if (a[this.orderProp] < b[this.orderProp]) {
+              return -1;
+            } else if ([b[this.orderProp] < a[this.orderProp]]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+    }
+    return phones;
+  }
+}
 
